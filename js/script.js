ec715d87be99cb48ef32317f5e30666fcee84240
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-25 16:00:25 +0100

    [UPD] Need a fix color

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 const lampadina = document.querySelector("img");
-const pulsante =
+const pulsante =document.getElementById("cambia-scritta")
+
+pulsante.addEventListener("click", function(){
+    if(lampadina.src.includes("white_lamp.png")){
+        lampadina.src = "./img/yellow_lamp.png";
+        pulsante.textContent = "Spegni";
+    }
+    else{
+        lampadina.src = "./img/white_lamp.png";
+        pulsante.textContent = "Accendi";
+    }
+
+    
+})
